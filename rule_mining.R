e049d9e2a3ccc28c02a0afad39568225dc302b7b
ntopper <nicktopper@gmail.com>
2015-12-15 19:50:36 -0500

    usual cast stuff is done

--- rule_mining.R
+++ rule_mining.R
@@ -1,37 +1,17 @@
 library(arules)
 
 cat("importing data\n")
-ususalSuspects <- read.csv("moviedb_data/UsualSuspects.csv")
+ususalSuspects <- read.csv("moviedb_data/UsualSuspects.csv", na.strings=c(" ", ""))
+ususalSuspects = na.omit(ususalSuspects)
 
-cat("mining all rules\n")
-rules <- apriori(ususalSuspects)
-print(inspect(rules))
-
-cat("\n")
 cat("\n")
 cat("mining all rules where Support = 0.01 and Confidence = 0.90\n")
-rules <- apriori(ususalSuspects, parameter = list(support = 0.01, confidence = 0.9))
+rules <- apriori(ususalSuspects, parameter = list(support = 0.0002, confidence = 0.0))
 
-cat("\n")
 cat("\n")
 cat("sorting by lift\n")
 rules.sorted = sort(rules, by="lift")
 
-cat("\n")
-cat("\n")
-cat("pruning redundant rules\n")
-subset.matrix <- is.subset(rules.sorted, rules.sorted)
-subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
-redundant <- colSums(subset.matrix, na.rm=T) >= 1
-rules.pruned <- rules.sorted[!redundant]
+rules.subset <- subset(rules.sorted, (rhs %in% paste0("Actor=", unique(ususalSuspects$Actor))))
 
-cat("\n")
-cat("\n")
-print(inspect(rules.pruned))
-
-
-cat("\n")
-cat("\n")
-cat("Creating subset where RHS is actor\n")
-rules.subset <- subset(rules, (rhs %in% paste0("actor=", unique(ususalSuspects$actor))))
-print(inspect(rules.subset))
+print(inspect(head(rules.subset,n=50)))
